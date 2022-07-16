ad4e8900ae83bb5209637fbbeb73cf23273a931d
Ashu Singh <cashusingh2@gmail.com>
2022-07-16 11:02:30 +0530

    Practice

--- Graphs/Hard/Water and jug.cpp
+++ Graphs/Hard/Water and jug.cpp
@@ -59,3 +59,15 @@ public:
         return false;
     }
 };
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int z)
+    {
+        if(z==0)
+            return true;
+        if(z>x+y)
+            return false;
+        int g=gcd(x,y);
+        return z%g==0;
+    }
+};
